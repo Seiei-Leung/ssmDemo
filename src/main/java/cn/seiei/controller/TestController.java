fc8d21597d1a2c0e8c34509d63604afa4266235b
AB786883603 <786883603@qq.com>
2018-08-10 00:02:06 +0800

    controller 添加上传图片

--- src/main/java/cn/seiei/controller/TestController.java
+++ src/main/java/cn/seiei/controller/TestController.java
@@ -1,14 +1,21 @@
 package cn.seiei.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.alibaba.fastjson.JSONArray;
 
@@ -54,5 +61,19 @@ public class TestController {
 	public int insertMsg(@RequestBody Student student) {
 		int result = studentService.insertMsg(student);
 		return result;
+	}
+	
+	// 上传图片
+	@RequestMapping(value="/file/upload", method=RequestMethod.POST)
+	public String upload(HttpServletRequest request,@RequestParam("pictureFile") MultipartFile pictureFile) throws IllegalStateException, IOException {
+		//使用UUID给图片重命名，并去掉四个“-”
+		String name = UUID.randomUUID().toString().replaceAll("-", "");
+		//获取文件的扩展名
+		if (pictureFile != null) {
+			String ext = FilenameUtils.getExtension(pictureFile.getOriginalFilename());
+			pictureFile.transferTo(new File("F:\\115\\"+name + "." + ext));
+			System.out.println(ext);
+		}
+		return "helloworld";
 	}
 }
