12187d7b59dd8f04e5fb9a301076385dff021fbc
AB786883603 <786883603@qq.com>
2018-08-10 23:49:11 +0800

    增加H5 formdata 实现文件上传

--- src/main/java/cn/seiei/controller/TestController.java
+++ src/main/java/cn/seiei/controller/TestController.java
@@ -64,17 +64,33 @@ public class TestController {
 	}
 	
 	// 上传图片
-	@RequestMapping(value="/file/upload", method=RequestMethod.POST)
-	public String upload(HttpServletRequest request,@RequestParam("pictureFile") MultipartFile pictureFile) throws IllegalStateException, IOException {
-		//使用UUID给图片重命名，并去掉四个“-”
-		String name = UUID.randomUUID().toString().replaceAll("-", "");
-		//获取文件的扩展名
-		if (pictureFile != null) {
-			String ext = FilenameUtils.getExtension(pictureFile.getOriginalFilename());
-			pictureFile.transferTo(new File("F:\\115\\"+name + "." + ext));
-			System.out.println(pictureFile.getOriginalFilename());
-			System.out.println(ext);
+		@RequestMapping(value="/file/upload", method=RequestMethod.POST)
+		public String upload(HttpServletRequest request,@RequestParam("pictureFile") MultipartFile pictureFile) throws IllegalStateException, IOException {
+			//使用UUID给图片重命名，并去掉四个“-”
+			String name = UUID.randomUUID().toString().replaceAll("-", "");
+			//获取文件的扩展名
+			if (pictureFile != null) {
+				String ext = FilenameUtils.getExtension(pictureFile.getOriginalFilename());
+				pictureFile.transferTo(new File("F:\\115\\"+name + "." + ext));
+				System.out.println(pictureFile.getOriginalFilename());
+				System.out.println(ext);
+			}
+			return "helloworld";
 		}
-		return "helloworld";
-	}
+		
+		
+		// 使用Html5 FormData实现多文件上传
+		@RequestMapping(value="/file/upload2", method=RequestMethod.POST)
+		public String upload2(HttpServletRequest request,@RequestParam("pictureFile") List<MultipartFile> pictureFiles) throws IllegalStateException, IOException {
+			
+			//获取文件的扩展名
+			for (MultipartFile file:pictureFiles) {
+				//使用UUID给图片重命名，并去掉四个“-”
+				String name = UUID.randomUUID().toString().replaceAll("-", "");
+	            String fileName = file.getOriginalFilename(); //获取文件名
+	            file.transferTo(new File("F:\\115\\"+name + "." + fileName));
+	            
+	        }
+			return "helloworld";
+		}
 }
